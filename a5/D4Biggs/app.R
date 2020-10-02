cf4176818a370ed96095516f02bdb706b10d29e8
Loi <loibucket@users.noreply.github.com>
2020-10-01 21:55:47 -0400

    a5

---
+++ a5/D4Biggs/app.R
@@ -0,0 +1,101 @@
+#Starter file for any Shiny dashboard app
+#This should replace the default app.r that displays Old Faithful data
+library(shiny)
+library(shinydashboard)
+library(shinyWidgets)
+library(DT)
+#The user interface
+header <- dashboardHeader(title = "D4 According to Biggs")
+sidebar <- dashboardSidebar(
+  width = 100,
+  actionButton("btninit", "Initialize"),
+  actionButton("btni","Apply i"),
+  actionButton("btnr","Apply r"),
+  actionButton("btns","Apply s"),
+  actionButton("btnt","Apply t"),
+  actionButton("btnw","Apply w"),
+  actionButton("btnx","Apply x"),
+  actionButton("btny","Apply y"),
+  actionButton("btnz","Apply z")
+)
+body <- dashboardBody(
+  fluidRow(
+    column(
+      width = 12,
+      plotOutput("configs", height = 200)
+    )
+  ),
+  fluidRow(
+    column(
+      width = 4,
+      plotOutput("square", height = 300)
+    ),
+    column(
+      width = 8,
+      dataTableOutput("multable")
+#      tableOutput("multable")
+    )
+  )
+)
+ui <- dashboardPage(header, sidebar, body, skin="yellow")
+
+#Functions that implement the mathematics
+source("d4calc.R")
+
+
+
+#Functions that read the input and modify the output and input
+server <- function(session, input, output) {
+  #Variables that are shared among server functions
+  D4DF <- D4.makeDataFrame()
+  config <- "ABCD"
+    #Initialization
+  output$configs <- renderPlot(D4.showConfigs(D4DF))
+  output$square <- renderPlot(D4.showSquare(config))
+  tbl <-outer(D4DF$name,D4DF$name,vD4.multiply,DF=D4DF)
+  colnames(tbl) <- D4DF$name
+  rownames(tbl) <- D4DF$name 
+  #Use options to suppress the fancy controls
+  output$multable <- renderDataTable(tbl, options = list(dom = "t"))
+#  output$multable <- renderTable(tbl, rownames = TRUE)
+    #Functions that respond to events in the input
+  observeEvent(input$btninit,{
+    config <<- "ABCD"
+    output$square <- renderPlot(D4.showSquare(config))
+  })
+  observeEvent(input$btni,{
+      config <<- D4.apply("i",config)
+      output$square <- renderPlot(D4.showSquare(config))
+  })
+  observeEvent(input$btnr,{
+      config <<- D4.apply("r",config)
+      output$square <- renderPlot(D4.showSquare(config))
+  })
+  observeEvent(input$btns,{
+      config <<- D4.apply("s",config)
+      output$square <- renderPlot(D4.showSquare(config))
+  })
+  observeEvent(input$btnt,{
+    config <<- D4.apply("t",config)
+    output$square <- renderPlot(D4.showSquare(config))
+  })
+  observeEvent(input$btnw,{
+    config <<- D4.apply("w",config)
+    output$square <- renderPlot(D4.showSquare(config))
+  })
+  observeEvent(input$btnx,{
+      config <<- D4.apply("x",config)
+      output$square <- renderPlot(D4.showSquare(config))
+  })
+  observeEvent(input$btny,{
+      config <<- D4.apply("y",config)
+      output$square <- renderPlot(D4.showSquare(config))
+  })
+  observeEvent(input$btnz,{
+      config <<- D4.apply("z",config)
+      output$square <- renderPlot(D4.showSquare(config))
+  })
+}
+
+#Run the app
+shinyApp(ui = ui, server = server)
---
+++ a5/D4Biggs/d4calc.R
@@ -0,0 +1,97 @@
+#d4calc.R - Symmetries of the equilateral triangle
+
+D4.makeDataFrame <- function() {
+  DF <- data.frame(name=rep("",8),cfg=rep("",8),stringsAsFactors = FALSE)
+  DF[1,] <- c("i","ABCD")
+  DF[2,] <- c("r","DABC")
+  DF[3,] <- c("s","BCDA")
+  DF[4,] <- c("t","CDAB")
+  DF[5,] <- c("w","BADC")
+  DF[6,] <- c("x","DCBA")
+  DF[7,] <- c("y","ADCB")
+  DF[8,] <- c("z","CBAD")
+  return(DF)
+}
+
+shift <- 2
+
+#BiggsDF <- D4.makeDataFrame()
+#BiggsDF
+D4.showConfigs <- function(DF) {
+  par(mar=c(1,1,1,1))
+  plot(NULL,xlim=c(0,18),ylim = c(-1,3), asp = 1, axes = FALSE)
+  for (i in 0:7) {
+    points(c(0,0,2,2,0)+3*i-shift,c(0,2,2,0,0),type = "l")
+    lbl <- strsplit(DF[i+1,2],"")[[1]]
+    s = 0.2
+    text(c(0+s,0+s,2-s,2-s)+3*i-shift,c(0+s,2-s,2-s,0+s),lbl)
+    text(1+3*i-shift,-0.5,DF[i+1,1])
+    ext = 0.2
+    segments(10-ext,1,x1=12+ext,y1=1,lty = 2) #w
+    segments(14,0-ext,x1=14,y1=2+ext,lty = 2) #x
+    segments(16-ext,0-ext,x1=18+ext,y1=2+ext,lty = 2) #y
+    segments(19-ext,2+ext,x1=21+ext,y1=0-ext,lty = 2) #z
+  }
+  "rotations label"
+  text(2,1,"cw90")
+  text(5,1,"cw270")
+  text(8,1,HTML("cw180"))
+}
+#D4.showConfigs(BiggsDF)
+
+D4.showSquare <- function(cfg){
+  par(mar=c(1,1,1,1))
+  plot(NULL,xlim=c(0,3),ylim = c(-1,2), asp = 1, axes = FALSE)
+  points(c(0,0,2,2,0),c(0,2,2,0,0),type = "l", lwd = 2)
+  lbl <- strsplit(cfg,"")[[1]]
+  s = 0.1
+  text(c(0+s,0+s,2-s,2-s),c(0+s,2-s,2-s,0+s),lbl)
+}
+#D4.showSquare("ABCD")
+
+#a is one of the Biggs symbols for an operation.
+
+#The return value is the new configuration
+D4.apply <- function(a,cfg){
+  v <-strsplit(cfg,"")[[1]]   #select first component of list
+  w <- switch(a,
+              "i" = v,
+              "r" = c(v[4],v[1],v[2],v[3]),
+              "s" = c(v[2],v[3],v[4],v[1]),
+              "t" = c(v[3],v[4],v[1],v[2]),
+              "w" = c(v[2],v[1],v[4],v[3]),
+              "x" = c(v[4],v[3],v[2],v[1]),
+              "y" = c(v[1],v[4],v[3],v[2]),
+              "z" = c(v[3],v[2],v[1],v[4])
+  )
+  s <- paste(w,collapse="") 
+  return(s)
+  # DF[1,] <- c("i","ABCD") 1234
+  # DF[2,] <- c("r","DABC") 4123
+  # DF[3,] <- c("s","BCDA") 2341
+  # DF[4,] <- c("t","CDAB") 3412
+  # DF[5,] <- c("w","BADC") 2143
+  # DF[6,] <- c("x","DCBA") 4321
+  # DF[7,] <- c("y","ADCB") 1432
+  # DF[8,] <- c("z","CBAD") 3214
+}
+D4.apply("r","ABCD")
+
+
+D4.multiply <- function(DF,a,b){
+  #Look up the name, which occurs once and only once
+  idx <- which.max(DF$name==b)
+  #Find the corresponding configuration
+  cfg <- DF$cfg[idx]
+  #Apply the group operation to it
+  newcfg <- D4.apply(a,cfg)
+ # Look up the configuration
+  idx <- which.max(DF$cfg==newcfg)
+  return (DF$name[idx])
+}
+#D4.multiply(BiggsDF,"r","r")
+
+#To use this with outer() we must vectorize it
+vD4.multiply <- Vectorize(D4.multiply,c("a","b"))
+#outer(c("i","r","s","x","y","z"),c("i","r","s","x","y","z"),"vD4.multiply", DF = BiggsDF)
+
